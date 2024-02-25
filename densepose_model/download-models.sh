299046f2a24a4042215266f1e02a36fbc183f32b
Sanjay <sanjaysusi29@gmail.com>
2024-02-25 15:20:59 +0530

    changes

--- densepose_model/download-models.sh
+++ densepose_model/download-models.sh
@@ -1,2 +1,4 @@
+mkdir ./densepose_model/models
+mkdir ./densepose_model/model_configs
 curl https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl -o ./densepose_model/models/model_final_162be9.pkl
 curl https://raw.githubusercontent.com/facebookresearch/detectron2/main/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml -o ./densepose_model/model_configs/densepose_rcnn_R_50_FPN_s1x.yaml
