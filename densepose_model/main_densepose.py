770fa3b33dcaaf11d42c6ca95122f08cf434caec
Sanjay <sanjaysusi29@gmail.com>
2024-02-25 15:01:36 +0530

    add schp api

---
+++ densepose_model/main_densepose.py
@@ -0,0 +1,87 @@
+import os
+import argparse
+import cv2
+import logging
+import torch
+import numpy as np
+from pathlib import Path
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from densepose import add_densepose_config
+from densepose.vis.extractor import (
+    DensePoseResultExtractor,
+)
+from densepose.vis.densepose_results import (
+    DensePoseResultsFineSegmentationVisualizer as Visualizer,
+)
+
+class GetLogger:
+    @staticmethod
+    def logger(name):
+        logging.basicConfig(
+            level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+        )
+        return logging.getLogger(name)
+
+
+class Predictor:
+    def __init__(self, config_path, model_weights):
+        cfg = get_cfg()
+        add_densepose_config(cfg)
+        self.default_config_path = config_path
+        cfg.merge_from_file(self.default_config_path)
+        cfg.MODEL.WEIGHTS = model_weights
+        cfg.MODEL.DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # Adjust as needed
+        self.predictor = DefaultPredictor(cfg)
+        self.extractor = DensePoseResultExtractor()
+        self.visualizer = Visualizer()
+
+    def predict(self, frame):
+        with torch.no_grad():
+            outputs = self.predictor(frame)["instances"]
+        outputs = self.extractor(outputs)
+
+        out_frame = frame.copy()
+        out_frame_seg = np.zeros(out_frame.shape, dtype=out_frame.dtype)
+
+        self.visualizer.visualize(out_frame, outputs)
+        self.visualizer.visualize(out_frame_seg, outputs)
+
+        return (out_frame, out_frame_seg)
+def build_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_path", type=str)
+    parser.add_argument("--output_path", type=str)
+    parser.add_argument("--config_path", type=str)
+    parser.add_argument("--weights", type=str)
+    args = parser.parse_args()
+    return args
+
+def main(input_path, output_path, config_path=None, weights=None):
+    if config_path is None:
+        config_path = '/home/pc/Documents/dress-up-api/densepose/model_configs/densepose_rcnn_R_50_FPN_s1x.yaml' #change it accordingly
+    if weights is None:
+        weights = '/home/pc/Documents/dress-up-api/densepose/models/model_final_162be9.pkl' #change it accordingly
+
+    logger = GetLogger.logger(__name__)
+    predictor = Predictor(config_path=config_path, model_weights=weights)
+    image = cv2.imread(input_path)
+    
+    if image is None:
+        logger.error(f"Could not read the image: {input_path}")
+        return
+    #processing
+    out_frame, out_frame_seg = predictor.predict(image)
+    input_filename, input_extension = os.path.splitext(os.path.basename(input_path))
+
+    #saving
+    cv2.imwrite(output_path, out_frame_seg)
+    print(f"DensePose image saved at {output_path}")
+    return output_path
+ 
+
+if __name__ == "__main__":
+    args = build_args()
+    main(args.input_path, args.output_path, args.config_path, args.weights)
+    
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from main_densepose import main as densepose
+from densepose_model.main_densepose import main as densepose
+from schp.main_schp import main as schp
 
-dense_path = densepose(input_path='image.jpg',output_path='densepose.jpg',weights='densepose_model/models/model_final_162be9.pkl',config_path='densepose_model/model_configs/densepose_rcnn_R_50_FPN_s1x.yaml')
+dense_path = densepose(input_path='test/image.jpg', 
+                       output_path='test/densepose.jpg', 
+                       weights='densepose_model/models/model_final_162be9.pkl', 
+                       config_path='densepose_model/model_configs/densepose_rcnn_R_50_FPN_s1x.yaml'
+                    )
+
+schp_path = schp(input_path='test/image.jpg',
+                 output_path='test/schp.png',
+                 weights='./schp/pretrain_model/exp-schp-201908261155-lip.pth'
+            )
+
+
+
 
