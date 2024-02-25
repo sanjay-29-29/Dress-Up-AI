579e7c0fd6535e58a9cc49817299962b21119c91
Sanjay <sanjaysusi29@gmail.com>
2024-02-25 18:41:27 +0530

    changes to filestructure

---
+++ STVN/README.md
@@ -0,0 +1,22 @@
+# StableVITON: Learning Semantic Correspondence with Latent Diffusion Model for Virtual Try-On
+This repository is a fork of the official implementation of [StableVITON](https://arxiv.org/abs/2312.01725)
+
+> **StableVITON: Learning Semantic Correspondence with Latent Diffusion Model for Virtual Try-On**<br>
+> [Jeongho Kim](https://scholar.google.co.kr/citations?user=ucoiLHQAAAAJ&hl=ko), [Gyojung Gu](https://www.linkedin.com/in/gyojung-gu-29033118b/), [Minho Park](https://pmh9960.github.io/), [Sunghyun Park](https://psh01087.github.io/), [Jaegul Choo](https://sites.google.com/site/jaegulchoo/) 
+
+[[Arxiv Paper](https://arxiv.org/abs/2312.01725)]&nbsp;
+[[Website Page](https://rlawjdghek.github.io/StableVITON/)]&nbsp;
+
+For more information, refer to the `README.md` of the original repository.
+
+## Environment
+It was tested on:
+* Ubuntu 22.04
+* CUDA version 11.7
+* Python 3.10
+```bash
+conda env create -f environment.yaml
+```
+
+## License
+Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 from dpm.main_densepose import main as densepose
 from schp.main_schp import main as schp
 from openpose.main import main as op
-
-dense_path = densepose(input_path='./test/image.jpg', 
+from STVN.main import main as stv
+from agmp.agnostic_map import 
+densepose(input_path='./test/image.jpg', 
                        output_path='./test/densepose.jpg', 
                        weights='./densepose_model/models/model_final_162be9.pkl', 
                        config_path='./densepose_model/model_configs/densepose_rcnn_R_50_FPN_s1x.yaml'
                     )
 
-schp_path = schp(input_path='./test/image.jpg',
+schp(input_path='./test/image.jpg',
                  output_path='./test/schp.png',
                  weights='./schp/pretrain_model/exp-schp-201908261155-lip.pth'
             )
             
-openpose_image_path, openpose_json_path = op(
 
+op(
 
-
-
-
-main(config_path='./StableVITON/configs/VITON512.yaml', data = '', output_path='output', weights='./StableVITON/ckpts/VITONHD.ckpt')
+StableVITON_out = stv(config_path='./StableVITON/configs/VITON512.yaml', 
+                      data = '', 
+                      output_path='output', 
+                      weights='./StableVITON/ckpts/VITONHD.ckpt'
+                  )
