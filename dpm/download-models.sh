d59b8d9d4edcc0c7c1fbdad4289ed8932520b2e0
Sanjay <sanjaysusi29@gmail.com>
2024-02-27 14:24:52 +0530

    commit

--- dpm/download-models.sh
+++ dpm/download-models.sh
@@ -1,5 +1,5 @@
 mkdir ./densepose_model/models
 mkdir ./densepose_model/model_configs
-curl https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl -o ./densepose_model/models/model_final_162be9.pkl
-curl https://raw.githubusercontent.com/facebookresearch/detectron2/main/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml -o ./densepose_model/model_configs/densepose_rcnn_R_50_FPN_s1x.yaml
-curl https://raw.githubusercontent.com/facebookresearch/detectron2/main/projects/DensePose/configs/Base-DensePose-RCNN-FPN.yaml -o ./densepose_model/model_configs/Base-DensePose-RCNN-FPN.yaml
+curl https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl -o ./dpm/models/model_final_162be9.pkl
+curl https://raw.githubusercontent.com/facebookresearch/detectron2/main/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml -o ./dpm/model_configs/densepose_rcnn_R_50_FPN_s1x.yaml
+curl https://raw.githubusercontent.com/facebookresearch/detectron2/main/projects/DensePose/configs/Base-DensePose-RCNN-FPN.yaml -o ./dpm/model_configs/Base-DensePose-RCNN-FPN.yaml
