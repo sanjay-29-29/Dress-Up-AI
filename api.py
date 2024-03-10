8c9f9904506a32e7a10bab0c010b1ba40d4c7ea3
Sanjay <sanjaysusi29@gmail.com>
2024-03-10 20:31:13 +0530

    changes to api

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+#from main import run_tasks
 from PIL import Image
 import io
 import pymongo
@@ -7,11 +9,11 @@ from datetime import datetime
 
 app = FastAPI()
 
-client = pymongo.MongoClient("mongodb://localhost:27017/") 
+'''client = pymongo.MongoClient("mongodb://localhost:27017/") 
 db = client["dress_up_ai"] 
-collection = db["time_and_filename"] 
+collection = db["time_and_filename"]'''
 
-@app.post("/upload_image")
+@app.post("/change_cloth")
 async def upload_image(image: UploadFile = File(...), cloth: UploadFile = File(...)):
     cloth_contents = await cloth.read()
     image_contents = await image.read() 
@@ -22,23 +24,21 @@ async def upload_image(image: UploadFile = File(...), cloth: UploadFile = File(.
         with Image.open(io.BytesIO(image_contents)) as im_image:
            im_image.save("./test/image/image.jpg")
 
-        transaction_record = {
+        '''transaction_record = {
             "timestamp": datetime.now(),
             "image_filename": image.filename,
             "image_size": len(image_contents),
             "cloth_filename": cloth.filename,
             "cloth_size": len(cloth_contents)
         }
-        collection.insert_one(transaction_record)  
+        collection.insert_one(transaction_record)'''  
 
     except Exception as e:
         return {"message": f"Error processing the image: {str(e)}"}
 
-    return {
-        "filename": image.filename, 
-        "message": "Image uploaded successfully \n"}
+    run_tasks()
 
-
+    return FileResponse('./data/output/unpair/image_cloth.jpg', media_type='image/jpeg')
 
 
 
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from dpm.main_densepose import main as densepose
 from schp.main_schp import main as schp
-#from opse.main import main as op
+from opse.main import main as op
 from main_stbl import main as stv
 from agmp.agnostic_map import main as agmp
 import threading
 import argparse
+import jsonify
 
 argparser = argparse.ArgumentParser(description='Virtual Try-On')
 argparser.add_argument('--stv', type=str, default='./content/drive/MyDrive/VITONHD.ckpt', help='input directory')
 argparser.add_argument('--data', type=str, default='./data', help='input directory')
+argparser.add_argument('--api', type=bool, default=False, help='API Yes/No')
 args = argparser.parse_args()
+
+densepose_done = threading.Event()
+schp_done = threading.Event()
+agmp_done = threading.Event()
+op_done = threading.Event()
 
 def run_densepose():
     print("Starting densepose")
@@ -30,7 +37,7 @@ def run_schp():
     print("Done... SCHP")
     schp_done.set()
 
-'''def run_op():
+def run_op():
     print("Starting OpenPose")
     op(image_dir='./data/image',
        json_path='./data/test/openpose_json',
@@ -38,9 +45,10 @@ def run_schp():
        model_path='./opse/models'
       )
     print("Done... OpenPose")
-    op_done.set()'''
+    op_done.set()
 
 def run_agmp():
+    op_done.wait()
     schp_done.wait()
     print("Starting AGMP")
     agmp(data_path = args.data + '/test',
@@ -51,8 +59,12 @@ def run_agmp():
     agmp_done.set()
     
 def run_stv():
-    #op_done.wait()
+
+    ''' Runs StableVITON 
+        args: No args required'''
+    
     global args
+    op_done.wait()
     densepose_done.wait()
     schp_done.wait()
     agmp_done.wait()
@@ -64,29 +76,67 @@ def run_stv():
        )
     print("Done...")
 
-if __name__ == "__main__":
-    densepose_done = threading.Event()
-    schp_done = threading.Event()
-    agmp_done = threading.Event()
-    #op_done = threading.Event()
+def run_tasks():
 
     densepose_thread = threading.Thread(target=run_densepose)
     schp_thread = threading.Thread(target=run_schp)
     agmp_thread = threading.Thread(target=run_agmp)
     stv_thread = threading.Thread(target=run_stv)
-    #op_thread = threading.Thread(target=run_op)
+    op_thread = threading.Thread(target=run_op)
 
     densepose_thread.start()
     schp_thread.start()
     agmp_thread.start()
     stv_thread.start()
-    #op_thread.start()
 
     densepose_thread.join()
     schp_thread.join()
     agmp_thread.join()
     stv_thread.join()
-    #op_thread.join()
+
+    return jsonify({'message': 'All tasks completed!'}), 200
+
+if __name__ == "__main__":
+               
+    densepose_done = threading.Event()
+    schp_done = threading.Event()
+    agmp_done = threading.Event()
+    op_done = threading.Event()
+
+    densepose_thread = threading.Thread(target=run_densepose)
+    schp_thread = threading.Thread(target=run_schp)
+    agmp_thread = threading.Thread(target=run_agmp)
+    stv_thread = threading.Thread(target=run_stv)
+    op_thread = threading.Thread(target=run_op)
+
+    densepose_thread.start()
+    schp_thread.start()
+    agmp_thread.start()
+    stv_thread.start()
+    op_thread.start()
+
+    densepose_thread.join()
+    schp_thread.join()
+    agmp_thread.join()
+    stv_thread.join()
+    op_thread.join()
 
     print("All tasks completed!")
 
+
+
+
+"""prenitha.jpg p2511365.jpg
+prenitha.jpg Grey_Tshirt.jpg
+image.jpg Grey_Tshirt.jpg
+p2567770.jpg Grey_Tshirt.jpg
+istockphoto-1331281439-612x612.jpg Grey_Tshirt.jpg
+721968.jpg Grey_Tshirt.jpg
+image.jpg p2511365.jpg
+721968.jpg p2511365.jpg
+definitely_not_yellow_t-shirt_for_men_base_15_6_2023_700x933.jpg Grey_Tshirt.jpg
+definitely_not_yellow_t-shirt_for_men_base_15_6_2023_700x933.jpg p2511365.jpg
+12459542-1854906041274098.jpg Grey_Tshirt.jpg
+12459542-1854906041274098.jpg p2511365.jpg
+Lacoste-tshirts.jpg Grey_Tshirt.jpg
+Lacoste-tshirts.jpg p2511365.jpg"""
